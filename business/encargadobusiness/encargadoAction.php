2eb9ebbddfbf5ffe5e136467120725ffe6a8b043
JEstiben <31193316+JEstiben@users.noreply.github.com>
2018-01-30 18:27:11 -0600

    Encargado
    Se agrego los nuevos atributos a encargadoAction.

--- business/encargadobusiness/encargadoAction.php
+++ business/encargadobusiness/encargadoAction.php
@@ -4,17 +4,19 @@ include './encargadoBusiness.php';
 
 if (isset($_POST['actualizar'])) {
 
-    if (isset($_POST['encargadoId']) && isset($_POST['encargadoNombreCompleto']) && isset($_POST['encargadoDireccion']) && isset($_POST['encargadoEstado'])) {
+    if (isset($_POST['encargadoId']) && isset($_POST['encargadoNombreCompleto']) && isset($_POST['encargadoDireccion']) && isset($_POST['encargadoCorreo']) && isset($_POST['encargadoPueblo']) && isset($_POST['encargadoEstado'])) {
             
         $encargadoId = $_POST['encargadoId'];
         $encargadoNombreCompleto = $_POST['encargadoNombreCompleto'];
         $encargadoDireccion = $_POST['encargadoDireccion'];
+        $encargadoCorreo = $_POST['encargadoCorreo'];
+        $encargadoPueblo = $_POST['encargadoPueblo'];
         $encargadoEstado = $_POST['encargadoEstado'];
         
 
-        if (strlen($encargadoNombreCompleto) > 0 && strlen($encargadoDireccion) > 0) {
-            if (!is_numeric($encargadoNombreCompleto)) {
-                $encargado = new encargado($encargadoId, $encargadoNombreCompleto, 0, $encargadoDireccion, $encargadoEstado);
+        if (strlen($encargadoNombreCompleto) > 0 && strlen($encargadoDireccion) > 0 && strlen($encargadoCorreo) > 0 && strlen($encargadoPueblo) > 0) {
+            if (!is_numeric($encargadoNombreCompleto) && !is_numeric($encargadoDireccion) && !is_numeric($encargadoPueblo)) {
+                $encargado = new encargado($encargadoId, $encargadoNombreCompleto, 0, $encargadoDireccion, $encargadoEstado, $encargadoCorreo, $encargadoPueblo);
 
                 $encargadoBusiness = new EncargadoBusiness();
 
@@ -53,17 +55,19 @@ if (isset($_POST['actualizar'])) {
     }//if si esta seteado el campo
 } else if (isset($_POST['insertar'])) {
 
-    if (isset($_POST['encargadoNombreCompleto']) && isset($_POST['encargadoTelefonos']) && isset($_POST['encargadoDireccion']) && isset($_POST['encargadoEstado'])) {
+    if (isset($_POST['encargadoNombreCompleto']) && isset($_POST['encargadoTelefonos']) && isset($_POST['encargadoDireccion']) && isset($_POST['encargadoPueblo']) && isset($_POST['encargadoEstado'])) {
             
         $encargadoNombreCompleto = $_POST['encargadoNombreCompleto'];
         $encargadoTelefono = $_POST['encargadoTelefonos'];
         $encargadoDireccion = $_POST['encargadoDireccion'];
+        $encargadoCorreo = $_POST['encargadoCorreo'];
+        $encargadoPueblo = $_POST['encargadoPueblo'];
         $encargadoEstado = $_POST['encargadoEstado'];
         
-        if (strlen($encargadoNombreCompleto) > 0 && strlen($encargadoDireccion) > 0) {
-            if (!is_numeric($encargadoNombreCompleto)) {
+        if (strlen($encargadoNombreCompleto) > 0 && strlen($encargadoDireccion) > 0 && strlen($encargadoCorreo) > 0 && strlen($encargadoPueblo) > 0) {
+            if (!is_numeric($encargadoNombreCompleto) && !is_numeric($encargadoDireccion) && !is_numeric($encargadoPueblo)) {
                 
-                $encargado = new encargado(0, $encargadoNombreCompleto, $encargadoTelefono, $encargadoDireccion, $encargadoEstado);
+                $encargado = new encargado(0, $encargadoNombreCompleto, $encargadoTelefono, $encargadoDireccion, $encargadoEstado, $encargadoCorreo, $encargadoPueblo);
 
                 $encargadoBusiness = new EncargadoBusiness();
 
