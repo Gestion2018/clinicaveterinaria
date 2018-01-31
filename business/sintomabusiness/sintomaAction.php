f609b32af5c9ca494c55559120fdfa1c99a5c033
JEstiben <31193316+JEstiben@users.noreply.github.com>
2018-01-31 09:55:29 -0600

    Sintoma
    Se creo sintomaAction y sintomaBusiness.

---
+++ business/sintomabusiness/sintomaAction.php
@@ -0,0 +1,85 @@
+<?php
+
+include './sintomaBusiness.php';
+
+if (isset($_POST['actualizar'])) {
+
+    if ( isset($_POST['sintomaId']) && isset($_POST['sintomaNombre']) && isset($_POST['sintomaDescripcion']) && isset($_POST['sintomaEstado'])) {
+            
+        $sintomaId = $_POST['sintomaId'];
+        $sintomaNombre = $_POST['sintomaNombre'];
+        $sintomaDescripcion = $_POST['sintomaDescripcion'];
+        $sintomaEstado = $_POST['sintomaEstado'];
+        
+
+        if (strlen($sintomaId) > 0 && strlen($sintomaNombre) > 0 && strlen($sintomaEstado) > 0 ) {
+            if (!is_numeric($sintomaNombre)) {
+                $sintoma = new sintoma($sintoma, $sintomaNombre, $sintomaDescripcion, $sintomaEstado);
+
+                $sintomaBusiness = new sintomaBusiness();
+
+                $resultado = $sintomaBusiness->actualizarTBSintoma($sintoma);
+                if ($resultado == 1) {
+                    header("location: ../../view/sintomaView.php?success=updated");
+                } else {
+                    //echo $idSickness." - ".$bullName;
+                    header("location: ../../view/sintomaView.php?error=dbError");
+                }//if error a nivel de base
+            } else {
+                header("location: ../../view/sintomaView.php?error=numberFormat");
+            }//if si se igreso un numero
+        } else {
+            header("location: ../../view/sintomaView.php?error=emptyField");
+        }//if si se dejo en blanco
+    } else {
+        header("location: ../../view/sintomaView.php?error=error");
+    }//if si esta seteado el campo
+} else if (isset($_POST['eliminar'])) {
+
+    if (isset($_POST['sintomaId'])) {
+
+        $sintomaId = $_POST['sintomaId'];
+
+        $sintomaBusiness = new sintomaBusiness();
+        $resultado = $sintomaBusiness->eliminarTBSintoma($sintomaId);
+
+        if ($resultado == 1) {
+            header("location: ../../view/sintomaView.php?success=deleted");
+        } else {
+            header("location: ../../view/sintomaView.php?error=dbError");
+        }//if error a nivel de base
+    } else {
+        header("location: ../../view/sintomaView.php?error=error");
+    }//if si esta seteado el campo
+} else if (isset($_POST['insertar'])) {
+
+    if (isset($_POST['sintomaNombre']) && isset($_POST['sintomaDescripcion']) && isset($_POST['sintomaEstado'])) {
+            
+        $sintomaNombre = $_POST['sintomaNombre'];
+        $sintomaDescripcion = $_POST['sintomaDescripcion'];
+        $sintomaEstado = $_POST['sintomaEstado'];
+        
+        if (strlen($sintomaNombre) > 0 && strlen($sintomaEstado) > 0 ) {
+            if (!is_numeric($sintomaNombre)) {
+                $sintoma = new sintoma(0, $sintomaNombre, $sintomaDescripcion, $sintomaEstado);
+
+                $sintomaBusiness = new sintomaBusiness();
+
+                $resultado = $sintomaBusiness->insertarTBSintoma($sintoma);
+
+                if ($resultado == 1) {
+                    header("location: ../../view/sintomaView.php?success=inserted");
+                } else {
+                    header("location: ../../view/sintomaView.php?error=dbError");
+                }//if error a nivel de base
+            } else {
+                header("location: ../../view/sintomaView.php?error=numberFormat");
+            }//if si se igreso un numero
+        } else {
+            header("location: ../../view/sintomaView.php?error=emptyField");
+        }//if si se dejo en blanco
+    } else {
+        header("location: ../../view/sintomaView.php?error=error");
+    }//if si esta seteado el campo
+}//if accion
+?>
---
+++ business/sintomabusiness/sintomaBusiness.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+Entra en el if se realiza las siguientes operaciones, por que se toma
+la ruta desde el business, y entra en el else si no se realiza el crud por
+que se toma la ruta desde el view
+*/
+if (isset($_POST['eliminar']) || isset($_POST['insertar']) || isset($_POST['actualizar'])|| isset($_POST['obtener'])) {
+    include_once '../../data/sintomadata/sintomadata.php';
+}else {
+    include_once '../data/sintomadata/sintomadata.php';
+}
+class SintomaBusiness {
+
+    private $sintomaData;
+
+    public function SintomaBusiness() {
+        $this->sintomaData = new SintomaData();
+    }//constructor
+
+    public function insertarTBSintoma($sintoma) {
+        return $this->sintomaData->insertarSintoma($sintoma);
+    }//InsertarSintoma
+
+    public function actualizarTBSintoma($sintoma) {
+        return $this->sintomaData->actualizarSintoma($sintoma);
+    }//ActualizarSintoma
+
+    public function eliminarTBSintoma($sintomaId) {
+        return $this->sintomaData->eliminarSintoma($sintomaId);
+    }//EliminarSintoma
+
+    public function obtenerTBSintoma() {
+        return $this->aintomaData->obtenerSintomas();
+    }//ObtenerSintoma
+
+    public function obtenerActualizar($sintomaId) {
+        return $this->sintomaData->obtenerActualizar($sintomaId);
+    }//ObtenerActualizar
+    
+}//class
+
+?>
